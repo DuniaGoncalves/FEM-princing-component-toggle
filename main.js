11b78f7f6c8b0ab4da358a98503e2ca046ab5a8e
Dunia Goncalves <dgoncalves@everquote.com>
2023-03-20 02:48:01 -0400

    add functionality to keydown js but kinda broken toggle by clicking

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
 const el = document.querySelector('#switch');
 
-const toggle = (checked) => {  
+const toggleSwitch = (checked) => {
   const h2 = document.querySelectorAll('h2');
 
   if (checked != el.checked) {
-    el.click();
+    // el.click();
     h2.forEach((el)=> {
       el.classList.toggle('hide');
-    })
- }
-}
+    });
+  }
+};
 
-el.addEventListener('click', toggle);
+const handleKeyDown = (event) => {
+  if (event.keyCode === 13) { // 13 is the key code for the Enter key
+    el.checked = !el.checked;
+    toggleSwitch();
+  }
+};
+
+el.addEventListener('click', () => {
+  el.checked = !el.checked;
+  toggleSwitch();
+});
+
+document.addEventListener('keydown', handleKeyDown);
