defde20d5d8f87f27a181104f5d35d04ae88db49
Dunia Goncalves <dgoncalves@everquote.com>
2023-03-22 02:09:01 -0400

    WIP: toggle click acting funny

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
 const el = document.querySelector('#switch');
 
-const toggleSwitch = (checked) => {
+const toggleSwitch = () => {
   const h2 = document.querySelectorAll('h2');
-
-  if (checked != el.checked) {
-    // el.click();
-    h2.forEach((el)=> {
-      el.classList.toggle('hide');
-    });
-  }
+  // el.click();
+  h2.forEach((el)=> {
+    el.classList.toggle('hide');
+  });
 };
 
 const handleKeyDown = (event) => {
-  if (event.keyCode === 13) { // 13 is the key code for the Enter key
+  if (event.keyCode === 13) {
     el.checked = !el.checked;
     toggleSwitch();
   }
 };
 
-el.addEventListener('click', () => {
+const handleClick = () => {
   el.checked = !el.checked;
   toggleSwitch();
-});
+};
 
 document.addEventListener('keydown', handleKeyDown);
+el.addEventListener('click', handleClick);
+// el.addEventListener('onclick', handleClick);
